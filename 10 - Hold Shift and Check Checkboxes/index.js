0092d76b35dbefc6de45905cfc2c4cf463cfc445
slipova <sofiya.lipova@mail.mcgill.ca>
2022-07-19 19:24:31 -0400

    separate logic

---
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -0,0 +1,28 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+
+  //check if shift is down
+  let inBetween = false;
+
+  if (e.shiftKey && this.checked) {
+
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      };
+
+      if (inBetween) {
+        checkbox.checked = true;
+      };
+
+    });
+
+  }
+
+  lastChecked = this;
+};
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
