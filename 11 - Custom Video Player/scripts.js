f00a0380e5adeff2cdcd82f012b1223505a3ceb2
slipova <sofiya.lipova@mail.mcgill.ca>
2022-07-20 17:56:42 -0400

    update button when played/paused

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,6 +1,6 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
-
+const toggle = player.querySelector('.toggle');
 
 // toggle between play and pause
 
@@ -12,4 +12,13 @@ function togglePlay() {
   }
 };
 
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚'
+  toggle.textContent = icon;
+}
+
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
