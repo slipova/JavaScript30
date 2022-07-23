25edbc8a044e2adcbc2f1ee6c33262f46ee152bf
slipova <sofiya.lipova@mail.mcgill.ca>
2022-07-23 11:19:26 -0400

    implement function to handle volume/playbackrate

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,12 @@
+
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
+
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const ranges = player.querySelectorAll('.player__slider')
 
 // toggle between play and pause
 
@@ -22,9 +27,15 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
 toggle.addEventListener('click', togglePlay);
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
