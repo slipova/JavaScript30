28a6dc349d2fe4b83f01b968d247ac40798d8729
slipova <sofiya.lipova@mail.mcgill.ca>
2022-07-25 17:37:16 -0400

    adjust scrub to respond to mousedown/up

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -51,4 +51,9 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 toggle.addEventListener('click', togglePlay);
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));;
+
+let mousedown = false;
 progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
